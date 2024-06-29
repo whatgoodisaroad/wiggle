1c9047d93e7d0bc05b4541b53b1b2fbe98cb9075
Wyatt Allen <wyatt.allen0@gmail.com>
2024-06-29 13:50:09 -0700

    Kick cleanup

--- src/instrument/fmKick.ts
+++ src/instrument/fmKick.ts
@@ -1,4 +1,4 @@
-import { ModuleRef, Patch, WiggleContext } from '../WiggleContext';
+import { ModuleRef, WiggleContext } from '../WiggleContext';
 import { adsr } from '../module/adsr';
 import { attenuverter } from '../module/attenuverter';
 import { vca } from '../module/vca';
@@ -21,12 +21,25 @@ export function fmKick(
     fmPitchFactor?: number;
   }
 ): ModuleRef {
-  const pitchEnvelope = adsr(context, { attack: 0, decay: pitchDecay, gate });
-  const fmPitch = attenuverter(
-    context,
-    { source: pitchEnvelope, offset: frequency, gain: fmPitchFactor }
-  );
-  const osc = vco(context, { frequency: fmPitch, shape: 'sine' });
-  const levelEnvelope = adsr(context, { attack: 0.001, decay, gate });
-  return vca(context, { input: osc, gain: levelEnvelope });
+  return vca(context, {
+    input: vco(context, {
+      frequency: attenuverter(
+        context, {
+          source: adsr(context, {
+            attack: 0,
+            decay: pitchDecay,
+            gate,
+          }),
+          offset: frequency,
+          gain: fmPitchFactor,
+        }
+      ),
+      shape: 'sine',
+    }),
+    gain: adsr(context, {
+      attack: 0.001,
+      decay,
+      gate,
+    }),
+  });
 }
